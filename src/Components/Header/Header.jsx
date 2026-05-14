2f498002d8cda257dd5627cbc83d1fb5d0350328
uy <a.normuraev20@gmail.com>
2026-05-14 08:23:52 +0500

    header boshlandi

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+import group from '../assets/Group.png'
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <div className="conatiner">
+            <div className="header-container">
+
+            </div>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
