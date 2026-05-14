28d6aefcaeda0b116cbbe33c278946de02b7c7b0
uy <a.normuraev20@gmail.com>
2026-05-14 09:20:08 +0500

    header select

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,15 @@
 
-import React from 'react'
-
+import React, { useTranslation } from 'react'
  
 import group from "../../assets/Group.png";
+import i18n from '../../i18n';
+import { useTranslation } from 'react-i18next';
 
+
+const {t, i18n} = useTranslation
+const handleChangeLanguage =(event)=>{
+    i18n.changeLanguage(event.target.value)
+}
 const Header = () => {
   return (
     <div>
@@ -13,10 +19,10 @@ const Header = () => {
             <img src={group} alt="" />
 
             <div className="home">
-                <a href="#"></a>
+                <a href="#">{t("header.home")}</a>
             </div>
 
-        <select name="" id="">
+        <select name="" id=""onChange={handleChangeLanguage}>
             <option value="uz">uz</option>
             <option value="ru">rus</option>
             <option value="eng">eng</option>
