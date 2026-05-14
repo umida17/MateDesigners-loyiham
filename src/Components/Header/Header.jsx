a7caff57be5271b6182ed1612bec607b43ae527b
uy <a.normuraev20@gmail.com>
2026-05-14 09:42:00 +0500

    header select option berish

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,37 +1,37 @@
-
-import React, { useTranslation } from 'react'
- 
-import group from "../../assets/Group.png";
-import i18n from '../../i18n';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
+import group from "../../assets/Group.png";
 
-
-const {t, i18n} = useTranslation
-const handleChangeLanguage =(event)=>{
-    i18n.changeLanguage(event.target.value)
-}
 const Header = () => {
+  const { t, i18n } = useTranslation(); // Hookni chaqirish
+
+  const handleChangeLanguage = (event) => {
+    i18n.changeLanguage(event.target.value);
+  };
+
   return (
-    <div>
-      <header>
-        <div className="conatiner">
-            <div className="header-container">
-            <img src={group} alt="" />
+    <header>
+      <div className="container">
+        <div className="header-container">
+          <img src={group} alt="Logo" />
 
-            <div className="home">
-                <a href="#">{t("header.home")}</a>
-            </div>
+          <div className="home">
+            <a href="#">{t("header.Home")}</a>
+            <a href="#">{t("header.About")}</a>
+            <a href="#">{t("header.Services")}</a>
+            <a href="#">{t("header.Work")}</a>
+            <a href="#">{t("header.Blog")}</a>
+          </div>
 
-        <select name="" id=""onChange={handleChangeLanguage}>
+          <select onChange={handleChangeLanguage} value={i18n.language}>
             <option value="uz">uz</option>
             <option value="ru">rus</option>
             <option value="eng">eng</option>
-        </select>
-            </div>
+          </select>
         </div>
-      </header>
-    </div>
-  )
-}
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
