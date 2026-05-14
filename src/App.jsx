94b23df0f8d13f05966f8a092f6899f643f44be1
uy <a.normuraev20@gmail.com>
2026-05-14 10:57:21 +0500

    main bowwlandi stil

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import React from 'react'
 import Header from './Components/Header/Header'
+import Hero from './Components/Hero/Hero'
 
 const App = () => {
   return (
     <div>
       <Header/>
+
       <Hero/>
+
     </div>
   )
 }
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,71 @@
 import React from 'react'
 import './Hero.css';
+import Rectangle from "../../assets/Rectangle.png"
+import Rectang from "../../assets/Rectangle 2.png"
+import Rec from "../../assets/Rectangle 3.png"
+import daraxt from "../../assets/daraxt.png"
+import dala from "../../assets/dala.png"
+import bulut from "../../assets/bulut.png"
+
 
 const Hero = () => {
   return (
     <div>
       <div className="section1">
         <div className="container">
-            
+            <div className="section1-container">
+                <h2 className='are'>We are your MateDesigners</h2>
+                <div className="cards">
+                  <div className="card">
+                  <img className='harf' src={Rectangle} alt="" />
+                  <h3 className='ghost'>Ghost UXWriter</h3>
+                  <p className='all'>All of us, designers, at least once per career became an illustrator, copyrighter and only God knows whom else. Out of the question it’s useful and interesting experience, but when we have some time limits we need a special magic.</p>
+
+                  </div>
+
+                  <div className="card">
+                  <img className='harf' src={Rectang} alt="" />
+                  <h3 className='ghost'>Content Reel</h3>
+                  <p className='all'>All of us, designers, at least once per career became an illustrator, copyrighter and only God knows whom else. Out of the question it’s useful and interesting experience, but when we have some time limits we need a special magic.</p>
+
+                  </div>
+
+
+                 <div className="card">
+                  <img className='harf' src={Rec} alt="" />
+                  <h3 className='ghost'>Status Annotations</h3>
+                  <p className='all'>All of us, designers, at least once per career became an illustrator, copyrighter and only God knows whom else. Out of the question it’s useful and interesting experience, but when we have some time limits we need a special magic.</p>
+
+                  </div>
+
+                </div>
+
+                <div className="cards">
+                  <div className="card">
+                  <img className='harf' src={daraxt} alt="" />
+                  <h3 className='ghost'>Ghost UXWriter</h3>
+                  <p className='all'>All of us, designers, at least once per career became an illustrator, copyrighter and only God knows whom else. Out of the question it’s useful and interesting experience, but when we have some time limits we need a special magic.</p>
+
+                  </div>
+
+                  <div className="card">
+                  <img className='harf' src={dala} alt="" />
+                  <h3 className='ghost'>Content Reel</h3>
+                  <p className='all'>All of us, designers, at least once per career became an illustrator, copyrighter and only God knows whom else. Out of the question it’s useful and interesting experience, but when we have some time limits we need a special magic.</p>
+
+                  </div>
+
+
+                 <div className="card">
+                  <img className='harf' src={bulut} alt="" />
+                  <h3 className='ghost'>Status Annotations</h3>
+                  <p className='all'>All of us, designers, at least once per career became an illustrator, copyrighter and only God knows whom else. Out of the question it’s useful and interesting experience, but when we have some time limits we need a special magic.</p>
+
+                  </div>
+
+                </div>
+                
+            </div>
         </div>
       </div>
     </div>
