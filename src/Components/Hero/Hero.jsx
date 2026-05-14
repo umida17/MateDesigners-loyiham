1b832030480f4bd583ec5ae85e70c0000ca735b4
uy <a.normuraev20@gmail.com>
2026-05-14 10:35:23 +0500

    main bowwlandi

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="section1">
+        <div className="container">
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
