c1ad61f257a3fea1970c1ae44c1516e56fc310a3
uy <a.normuraev20@gmail.com>
2026-05-14 08:39:52 +0500

    header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,16 @@
 
 import React from 'react'
 
+ 
 import group from "../../assets/Group.png";
+
 const Header = () => {
   return (
     <div>
       <header>
         <div className="conatiner">
             <div className="header-container">
-
+            <img src={group} alt="" />
             </div>
         </div>
       </header>
