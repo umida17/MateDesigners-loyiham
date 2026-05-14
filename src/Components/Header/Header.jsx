9b00301d053979439eeb3bb251e35213b7c8f70b
uy <a.normuraev20@gmail.com>
2026-05-14 08:46:06 +0500

    header select

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -11,6 +11,16 @@ const Header = () => {
         <div className="conatiner">
             <div className="header-container">
             <img src={group} alt="" />
+
+            <div className="home">
+                <a href="#"></a>
+            </div>
+
+        <select name="" id="">
+            <option value="uz">uz</option>
+            <option value="ru">rus</option>
+            <option value="eng">eng</option>
+        </select>
             </div>
         </div>
       </header>
